d1781840304b8edd6a7a0d5c58ffce64fb10ad4a
Jacques De SAGAN <jacquesde@sagan.me>
2026-06-01 17:29:14 +0800

    update

--- csapi.d.ts
+++ csapi.d.ts
@@ -22254,6 +22254,7 @@ export interface PaneData {
 	host: string;
 	state?: string;
 	cloneFrom?: string;
+	options?: Record<string, string>;
 }
 export interface TabData {
 	id: string;
@@ -22351,9 +22352,9 @@ declare global {
 	var __CS_DISABLE_SHORTCUTS__: Set<string>;
 	/**
 	 * Focus the terminal with the given pane id.
-	 * @param paneId defaults to active terminal pane id.
+	 * @param tabOrPaneId defaults to active terminal pane id.
 	 */
-	function csFocus(paneId?: string): void;
+	function csFocus(tabOrPaneId?: string): void;
 	/**
 	 * Display a notification.
 	 * @param severity defaults to "info".
@@ -22367,10 +22368,23 @@ declare global {
 	 * Note we don't recommend putting password in connection string.
 	 * CozySSH does not log or store password anywhere but custom scripts are stored on server in plain text files.
 	 * So be careful with any secrets in custom scripts.
-	 * @param options.name Optional title for the new tab.
+	 * It's possible to append a optional query string in connection string, e.g `user@host?remoteCommand=bash`,
+	 * to set some optional session scope parameters. Available parameters:
+	 *   - `remoteCommand`: command to run on remote host
+	 *   - `cols`: initial columns of the terminal
+	 *   - `rows`: initial rows of the terminal
+	 *   - `noPublicKey`: if `1`, public key authentication will be disabled
+	 *   - `identity`: Manually set the identity (ssh private key) file path (on backend)
+	 *                 or contents that will be used for authentication in this session.
+	 * @param options.title Optional title for the new tab.
+	 * @param options.target Optional target tab id. The target tab must currently contains less than 4 panes.
+	 *                       Special values:
+	 *                       - `_blank` : open in new tab (default)
+	 *                       - `_self` : open in active tab
 	 */
 	function csOpen(target: HostData | string | (HostData | string)[], options?: {
-		name?: string;
+		title?: string;
+		target?: string;
 	}): void;
 	/**
 	 * Close a tab or pane.
